0dfbca416685909c7c48cd568374e6be9caa2c36
Chris Seagraves <christopher.seagraves@gmail.com>
2023-01-12 17:12:40 -0600

    removed error messages being sent to discord... 👀

--- src/servers/tepcott/commands/submit.rs
+++ src/servers/tepcott/commands/submit.rs
@@ -19,32 +19,32 @@ pub async fn submit(context: &Context, msg: &Message, mut split_message: Split<'
     let link = split_message.next();
 
     if time.is_none() || link.is_none() {                                       // check valid number of arguments
-        let _ = msg.channel_id.say(
-            &context.http, 
-            "Invalid number of arguments. Please use the following format: !submit <MM:SS.mmm> <weblink to your video/screenshot>"
-        )
-            .await;
+        // let _ = msg.channel_id.say(
+        //     &context.http, 
+        //     "Invalid number of arguments. Please use the following format: !submit <MM:SS.mmm> <weblink to your video/screenshot>"
+        // )
+        //     .await;
         return Ok(());
     }
 
     
     let time_format = Regex::new(r"^\d{1,2}:\d{2}\.\d{3}$").unwrap();
     if !time_format.is_match(time.unwrap()) {                              // check valid time format
-        let _ = msg.channel_id.say(
-            &context.http, 
-            "Invalid time format. Please use the following format: MM:SS.mmm"
-        )
-            .await;
+        // let _ = msg.channel_id.say(
+        //     &context.http, 
+        //     "Invalid time format. Please use the following format: MM:SS.mmm"
+        // )
+        //     .await;
         return Ok(());
     }
 
     let link_format = Regex::new(r"^(https?://)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$").unwrap();
     if !link_format.is_match(link.unwrap()) {                              // check valid link format
         // return error message if invalid link format
-        let _ = msg.channel_id.say(
-            &context.http,
-            "Invalid link format. Please provide a valid weblink.")
-            .await;
+        // let _ = msg.channel_id.say(
+        //     &context.http,
+        //     "Invalid link format. Please provide a valid weblink.")
+        //     .await;
         return Ok(());
     }
 
