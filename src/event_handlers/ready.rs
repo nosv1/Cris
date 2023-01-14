bd9fbb450e5146fcd69a1d294cb6deef69a0787a
Chris Seagraves <christopher.seagraves@gmail.com>
2023-01-14 14:48:05 -0600

    hotfix for import error

--- src/event_handlers/ready.rs
+++ src/event_handlers/ready.rs
@@ -1,9 +1,10 @@
 use serenity::model::gateway::Ready;
+use serenity::model::user::OnlineStatus;
 use serenity::prelude::Context;
 
 pub struct ReadyHandler;
 
-pub async fn ready(_ctx: Context, ready: Ready) {
+pub async fn ready(ctx: Context, ready: Ready) {
     println!("{} is connected!", ready.user.name);
     ctx.set_presence(None, OnlineStatus::Online).await;
 }
