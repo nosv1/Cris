7d893d7dff23d602f878edd8489bf4786404a7bb
Chris Seagraves <christopher.seagraves@gmail.com>
2023-01-29 14:51:03 -0600

    ignoring reserve command for now

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import src.event_handlers.on_ready as eh_on_ready
 
 from src.servers.phyner.phyner import GUILD_ID as phyner_guild_id
 from src.servers.tepcott.tepcott import GUILD_ID as tepcott_guild_id
-from src.servers.tepcott.commands import reserve as tepcott_reserve
+# from src.servers.tepcott.commands import reserve as tepcott_reserve
 from src.servers.tepcott.commands import updatedivs as tepcott_updatedivs
 from src.servers.tepcott.commands import startingorder as tepcott_startingorder
 
@@ -20,21 +20,30 @@ async def on_ready():
 
 ########################    COMMANDS    ########################
 
-@bot.slash_command(guild_ids=[tepcott_guild_id], description="Assign/unassign a reserve to/from a driver")
-async def reserve(ctx: discord.ApplicationContext):
-    """ """
+# @bot.slash_command(guild_ids=[tepcott_guild_id], description="Assign/unassign a reserve to/from a driver")
+# async def reserve(ctx: discord.ApplicationContext):
+#     """ """
 
-    await tepcott_reserve.reserve(ctx, bot)
+#     if bot.debug and not bot.is_developer(ctx.author):
+#         return
+
+#     await tepcott_reserve.reserve(ctx, bot)
 
 @bot.slash_command(guild_ids=[tepcott_guild_id], description="Update the division roles and nicknames of all participants based on the spreadsheet")
 async def updatedivs(ctx: discord.ApplicationContext):
     """ """
+
+    if bot.debug and not bot.is_developer(ctx.author):
+        return
 
     await tepcott_updatedivs.updatedivs(ctx, bot)
 
 @bot.slash_command(guild_ids=[tepcott_guild_id], description="Get the starting order for the current round.")
 async def startingorder(ctx: discord.ApplicationContext):
     """ """
+
+    if bot.debug and not bot.is_developer(ctx.author):
+        return
 
     await tepcott_startingorder.startingorder(ctx, bot)
 
